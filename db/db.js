14aed6a07ff99fe833375665a1cda012818a8866
Roshan Kumar Khadka <khadkaroshan715@gmail.com>
2026-02-12 14:19:53 +0545

    teaching

---
+++ db/db.js
@@ -0,0 +1,21 @@
+// const mongoose = require("mongoose");
+// const dotenv = require("dotenv")
+
+// dotenv.config();
+
+// const connectToDatabase = async() => {
+//     try {
+//          await mongoose.connect(process.env.DB_URL)
+//         console.log('DB is connected')
+//     } catch (error) {
+//         console.log(`Error in db connection is ${error}`)
+//     }
+// }
+
+// module.exports = connectToDatabase;
+
+
+
+
+
+
---
+++ routes/userRoute.js
@@ -0,0 +1,38 @@
+// const router = require("express").Router();
+// const userController = require("../controller/userController")
+
+// router.post("/register", userController.register)
+
+// module.exports = router
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
